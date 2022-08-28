aa58184af07fb385699db386d66716fc89145854
ChuNgocBinh96 <cnb96.cv@gmail.com>
2022-08-28 09:52:12 +0700

    add swagger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 require('express-async-errors');
+const swaggerUi = require('swagger-ui-express');
 const authRouter = require('./modules/auth/auth.router');
 const shopRouter = require('./modules/shop_account/shop_account.router');
 const productRouter = require('./modules/product/product.router');
@@ -18,6 +19,7 @@ app.use('/api/shop', shopRouter);
 app.use('/api/product', productRouter);
 app.use('/api/comment', commentRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api-docs', swaggerUi.serve);
 
 app.use(handleError);
 
